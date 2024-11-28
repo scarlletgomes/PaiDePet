2ffc2ecdfcb0091c02cd6a4b1b081ebaeccb49c0
Scarllet Gomes <86412432+scarlletgomes@users.noreply.github.com>
2024-11-28 19:02:58 -0300

    Função excluir endereço

--- PaiDePet/routers/enderecoRouter.js
+++ PaiDePet/routers/enderecoRouter.js
@@ -7,7 +7,9 @@ router.get('/endereco', enderecoController.enderecoCadastroView);
 router.post('/endereco', enderecoController.postEndereco);
 router.get('/enderecoCadastrado', enderecoController.getEnderecoView);
 router.get('/endereco/:id', enderecoController.editarEndereco);  
-router.post('/endereco/:id', enderecoController.atualizarEndereco);  
+router.post('/endereco/:id', enderecoController.atualizarEndereco);
+router.post('/excluir/:id', enderecoController.excluirEndereco);  
+router.get('/excluir/:id', enderecoController.excluirEndereco); 
 
 module.exports = router;
 
